3e5656477a6185680ca1d8a330856176c7d4bd09
HXMMXR <holla@deshawnjones.com>
2025-06-08 18:52:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+console.log("Hello, world!");
+new Typed('.hero-content h2', {
+  strings: ["Custom builds.", "Expert services.", "PCs tuned to perfection."],
+  typeSpeed: 50,
+  backSpeed: 25,
+  loop: true
+});
